8a7c2db41783929d8733131dd9b8a015cc4d988d
Sima Bilony <www.simabilony@gmail.com>
2024-11-18 16:31:03 +0300

    Custom Filter: Date Picker

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -73,7 +73,23 @@ class ProductResource extends Resource
                  Tables\Filters\SelectFilter::make('status')
                      ->options(self::$statuses),
                 Tables\Filters\SelectFilter::make('category')
-                    ->relationship('category', 'name')
+                    ->relationship('category', 'name'),
+                  Tables\Filters\Filter::make('created_at')
+                      ->form([
+                          Forms\Components\DatePicker::make('created_from'),
+                          Forms\Components\DatePicker::make('created_until'),
+                      ])
+                      ->query(function (Builder $query, array $data): Builder {
+                          return $query
+                              ->when(
+                                  $data['created_from'],
+                                  fn (Builder $query, $date): Builder => $query->whereDate('created_at', '>=', $date),
+                              )
+                              ->when(
+                                  $data['created_until'],
+                                  fn (Builder $query, $date): Builder => $query->whereDate('created_at', '<=', $date),
+                              );
+                      })
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
